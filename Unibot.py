920406cab7aa9535075fa748fb95aa439fb79308
Million Haileyesus <107280315+million-haileyesus@users.noreply.github.com>
2023-03-28 19:58:19 -0400

    WORKS BUT STILL NEEDS MORE INPUTS AND ACCURACY

--- Unibot.py
+++ Unibot.py
@@ -11,11 +11,15 @@ from fuzzywuzzy import fuzz
 with open("data.txt", "r", encoding = "UTF-8") as data_file:
     text = data_file.read()
     text = text.lower()
+    se_tokens = nltk.sent_tokenize(text)
+    wo_tokens = nltk.word_tokenize(text)
 
-def LemNormalize():
+
+def LemNormalize(words_list):
     lem = nltk.stem.WordNetLemmatizer()
     remove_punctuations = dict((ord(punct), None) for punct in string.punctuation)
-    tokens = nltk.word_tokenize(text.lower().translate(remove_punctuations))
+    words_string = ' '.join(words_list)
+    tokens = nltk.word_tokenize(words_string.lower().translate(remove_punctuations))
     lemmatized = [lem.lemmatize(token) for token in tokens]
     return lemmatized
 
@@ -27,46 +31,29 @@ def greetings(greeting_sentence):
         return random.choice(greeting_response)
 
 def response(user_response):
-    sentence_tokens = nltk.sent_tokenize(user_response)
+
     bot_response = ""
 
-    #if len(sentence_tokens) < 1:
-     #   bot_response = "I am unable to answer that question, sorry."
-      #  return bot_response
-
-    #idx = vals.argsort()[0][-2]
-    #flat = vals.flatten()
-    #flat.sort()
-    #req_tfidf = flat[-2]
-
-#    if(req_tfidf == 0):
- #       bot_response = bot_response + "I am unable to answer that question, sorry."
-  #  else:
-   #     bot_response = bot_response + sentence_tokens[idx] + " "
-
-    #return bot_response
-
-    TfidVec = TfidfVectorizer(tokenizer=lambda x :LemNormalize(), stop_words="english")
-    tfidf = TfidVec.fit_transform(sentence_tokens)
+    TfidVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words="english")
+    tfidf = TfidVec.fit_transform(se_tokens)
     vals = cosine_similarity(tfidf[-1], tfidf)
     similarity_scores = []
 
-    for i in range(len(sentence_tokens) - 1):
-        ratio = fuzz.token_sort_ratio(sentence_tokens[i], user_response)
+    for i in range(len(se_tokens) - 1):
+        ratio = fuzz.token_sort_ratio(se_tokens[i], user_response)
         weighted_score = vals[0][i] + (ratio / 100)
         similarity_scores.append(weighted_score)
+
     if not similarity_scores:
         bot_response = bot_response + "I am unable to answer that question, sorry."
 
     else:
         idx = similarity_scores.index(max(similarity_scores))
-        bot_response = sentence_tokens[idx]
+        bot_response = se_tokens[idx]
 
     return bot_response
 
 def chat_flow():
-    sentence_tokens = nltk.sent_tokenize(text)
-    word_tokens = nltk.word_tokenize(text)
 
     flag = True
     print("Bot: Hi there! How can I assist you today?")
@@ -75,7 +62,7 @@ def chat_flow():
         user_response = input("User: ")
         user_response = user_response.lower()
 
-        if(user_response == "bye"):
+        if("bye" in user_response):
             flag = False
             print("Bot: Goodbye!")
 
@@ -93,10 +80,10 @@ def chat_flow():
                     print("Bot: " + greetings(user_response))
 
                 else:
-                    # sentence_tokens.append(user_response)
-                    # word_tokens = word_tokens + nltk.word_tokenize(sentence_tokens)
-                    # final_words = list(set(word_tokens))
+                    se_tokens.append(user_response)
+                    wo_tokens.extend(nltk.word_tokenize(user_response))
+                    final_words = list(set(wo_tokens))
                     print(f"Bot: {response(user_response)}")
-                    # sentence_tokens.remove(user_response)
+                    se_tokens.remove(user_response)
 
 chat_flow()
