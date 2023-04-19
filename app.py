5d7fbc84d0705a7fbba3fd536f35aa3783409336
nhunewgnu <75112638+nhunewgnu@users.noreply.github.com>
2023-04-19 12:37:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from flask import Flask, render_template, request, jsonify
-from Unibot2 import chat_flow
-app = Flask(__name__)
+from flask import Flask, request, render_template, jsonify
+from Unibot import chat_flow
 
-@app.route("/")
-def index():
-    return render_template("unibot.html")
+unibot_app = Flask(__name__)
 
-@app.post("/predict")
-def predict():
-    text = request.get_json().get("message")
-    response = chat_flow(text)
-    message = {"answer": response}
-    return jsonify(message)
+unibot_app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@unibot_app.route('/')
+def home():
+    return render_template('unibot.html')
+
+@unibot_app.route('/chat_bot', methods=['POST'])
+def respond():
+    user_input = request.get_json()['message']
+    response = chat_flow(user_input)
+    return jsonify(response)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    unibot_app.run(host='0.0.0.1', port=5000)
