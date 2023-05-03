9c8cd848b86ac8d803811bb39570bef0867d3f84
Million Haileyesus <107280315+million-haileyesus@users.noreply.github.com>
2023-05-03 19:40:36 -0400

    Update Unibot.py

--- backend/Unibot.py
+++ backend/Unibot.py
@@ -1,12 +1,16 @@
-from spacy import load
 import nltk
-from nltk.corpus import stopwords
 import string
 import warnings
 import random
+import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from fuzzywuzzy import fuzz
+from nltk import porter
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+from spacy import load
+
 
 nlp = load("en_core_web_sm")
 stopwords_list = stopwords.words('english')
@@ -14,30 +18,27 @@ warnings.filterwarnings("ignore")
 
 def queries():
     with open("data.txt", "r", encoding = "UTF-8") as data_file:
-        data = data_file.read()
-        data = data.lower()
+        data = data_file.readlines()
+        data = [re.sub(r'[\n\t\r]+', ' ', line) for line in data]
     data_file.close()
 
     return data
 
-text = queries()
-sentence_tokens = nltk.sent_tokenize(text)
-word_tokens = nltk.word_tokenize(text)
-
-
-def LemNormalize(text):
-    lem = nltk.stem.WordNetLemmatizer()
+def preprocess_text(input_text):
+    lem = WordNetLemmatizer()
     remove_punctuations = dict((ord(punct), None) for punct in string.punctuation)
-    tokens = nltk.word_tokenize(text.lower())
-    filtered_tokens = [lem.lemmatize(token) for token in tokens if token.lower() not in stopwords_list]
-    filtered_text = ' '.join(filtered_tokens)
-    filtered_text = filtered_text.translate(remove_punctuations)
+    tokens = nltk.word_tokenize(input_text)
+    filtered_tokens = [lem.lemmatize(token) for token in tokens]
+    filtered_tokens = [token.translate(remove_punctuations) for token in filtered_tokens]
 
-    return filtered_text
+    return filtered_tokens
+
+text = queries()
+sentence_tokens = text
 
 def greetings(greeting_sentence):
     greeting_inputs = ["hello", "hi", "hey", "how is it going?", "how are you doing?", "what's up", "whats up", "hi there"]
-    greeting_response = ["Hello", "Hi", "Hey", "How is it going?", "How are you doing?"]
+    greeting_response = ["Hello", "Hi", "Hey", "Howdy", "How is it going?", "How are you doing?"]
 
     if greeting_sentence in greeting_inputs:
         return random.choices(greeting_response)[0]
@@ -51,36 +52,26 @@ def response(user_response):
 
     else:
         score = max(similar_scores)
-        if score < 0.78:
+
+        if score < 0.80:
             bot_response = bot_response + "I am unable to answer that question, sorry.\
                                             \nCould you be more specific?"
 
         else:
             idx = similar_scores.index(score)
-            bot_response = bot_response + sentence_tokens[idx].strip().capitalize()
-
-        #i = 0
-        #while len(similar_scores) != 0:
-         #   if i == 3:
-         #       break
-          #  idx = similar_scores.index(max(similar_scores))
-           # print(max(similar_scores))
-          #  bot_response = bot_response + " " + sentence_tokens[idx].strip().capitalize()
-           # i += 1
-            #similar_scores.remove(max(similar_scores))
+            bot_response = bot_response + text[idx].strip().capitalize()
 
     return bot_response
 
 def train(user_response):
-    TfidVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words=stopwords_list)
-    tfidf = TfidVec.fit_transform(sentence_tokens)
-    vals = cosine_similarity(tfidf[-1], tfidf)
-    vals = vals.flatten()
+    tfidVec = TfidfVectorizer(tokenizer=preprocess_text, stop_words=stopwords_list)
+    tfidf = tfidVec.fit_transform(sentence_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf).flatten()
     similarity_scores = []
 
     for i in range(len(sentence_tokens) - 1):
         ratio = fuzz.token_set_ratio(sentence_tokens[i], user_response)
-        weighted_score = vals[i] * (ratio / 100)
+        weighted_score = vals[i] + (ratio / 100)
         similarity_scores.append(weighted_score)
 
     return similarity_scores
