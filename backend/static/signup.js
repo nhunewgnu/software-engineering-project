873a77adb35e7d3e0845315154de764bfbe3b3a3
Million Seyoum Haileyesus <noillimsey@gmail.com>
2023-05-04 14:13:58 -0400

    Update

--- backend/static/signup.js
+++ backend/static/signup.js
@@ -4,4 +4,3 @@ document.getElementById("signup-form").addEventListener("submit", (event) => {
     // Replace this with server-side validation and data storage
     alert("Sign up successful! In a real-world application, you should store the user data in a secure database and implement proper authentication mechanisms.");
   });
-  
--- backend/static/unibot.js
+++ backend/static/unibot.js
@@ -1,3 +1,23 @@
+const messages = document.querySelector(".messages");
+const messageForm = document.querySelector("#message-form");
+const input = document.querySelector("#message");
+
+messageForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const message = input.value;
+  const newListItem = document.createElement("li");
+  const newParagraph = document.createElement("p");
+  newParagraph.innerText = message ? message : "Some error happened.";
+  newListItem.appendChild(newParagraph);
+  newListItem.classList.add("message", "user");
+  messages.appendChild(newListItem);
+  messages.scrollTop = messages.scrollHeight;
+  input.value = "";
+
+  // send message to some api
+  console.log(message);
+});
+
 const loginBtn = document.getElementById("login-btn");
 
 function sendMessage() {
@@ -7,8 +27,7 @@ function sendMessage() {
 
     // Display user message in chat box
     var userMsg = document.createElement("div");
-    userMsg.classList.add("item", "right");
-    userMsg.innerHTML = '<div class="msg"><p>' + messageText + '</p></div>';
+    userMsg.classList.add("messages", "right");
     chatBox.appendChild(userMsg);
 
     // Send message using AJAX
@@ -24,35 +43,35 @@ function sendMessage() {
             inputField.value = "";
         }
     };
+    console.log(xhr)
     xhr.send(JSON.stringify({message: messageText}));
 }
 
-var inputField = document.querySelector(".input");
-inputField.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        sendMessage();
-    }
-});
-
-loginBtn.addEventListener("click", () => {
-    window.location.href = "login.html";
-  });
-
-
-logoutBtn.addEventListener("click", () => {
-    localStorage.removeItem("username");
-    updateUI();
-  });
-
-  function updateUI() {
-    const username = localStorage.getItem("username");
-    if (username) {
-      userInfo.style.display = "block";
-      authOptions.style.display = "none";
-      usernameDisplay.textContent = username;
-    } else {
-      userInfo.style.display = "none";
-      authOptions.style.display = "block";
-    }
-  }
-
+//var inputField = document.querySelector(".input");
+//inputField.addEventListener("keypress", function(event) {
+//    if (event.key === "Enter") {
+//        sendMessage();
+//    }
+//});
+//
+//loginBtn.addEventListener("click", () => {
+//    window.location.href = "login.html";
+//  });
+//
+//
+//logoutBtn.addEventListener("click", () => {
+//    localStorage.removeItem("username");
+//    updateUI();
+//  });
+//
+//  function updateUI() {
+//    const username = localStorage.getItem("username");
+//    if (username) {
+//      userInfo.style.display = "block";
+//      authOptions.style.display = "none";
+//      usernameDisplay.textContent = username;
+//    } else {
+//      userInfo.style.display = "none";
+//      authOptions.style.display = "block";
+//    }
+//  }
